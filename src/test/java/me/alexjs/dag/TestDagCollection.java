1b2b6c93de7cdc5608e9d6af580c0526ecbdf7b0
Alex Sweeney <ajs1998@gmail.com>
2022-05-21 23:19:56 -0500

    i only really care about java 11 and 17

--- src/test/java/me/alexjs/dag/TestDagCollection.java
+++ src/test/java/me/alexjs/dag/TestDagCollection.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestDagCollection {
