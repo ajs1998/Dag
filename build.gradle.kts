ca31c3647984cd8bdacd30694fdb7a015fa6ed89
Alex Sweeney <ajs1998@gmail.com>
2023-07-10 23:52:42 -0500

    libs.versions.toml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    alias(libs.plugins.jreleaser)
 }
 
 group = "dev.alexjs"
@@ -11,10 +12,10 @@ repositories {
 
 dependencies {
 
-    implementation("com.google.guava:guava:31.1-jre")
+    implementation(libs.guava)
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation(libs.junit)
+    testRuntimeOnly(libs.junitEngine)
 
 }
 
